adbfb000e545054480e61a342a42f61b85ac5396
Sithija <your-email@example.com>
2026-05-07 12:35:51 +0530

    Chore : connected to mongodb database

---
+++ server.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'; // Shortcut to load .env immediately
+import express from 'express';
+import connectDB from './server/config/db.js'; 
+
+
+const app = express();
+app.use(express.json());
+
+
+
+const startServer = async () => {
+    try {
+        await connectDB(); 
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`🚀 Fuel QR Server running on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error("Critical failure:", err);
+    }
+};
+
+startServer();
---
+++ server/config/db.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const url = process.env.MONGO_URI;
+const client = new MongoClient(url);
+
+const dbName = 'FuelQR_DB'; 
+let db;
+
+
+export default async function connectDB() {
+    if (db) return db;
+    try {
+        console.log("Attempting to connect to MongoDB Atlas...");
+        
+        await client.connect();
+        
+        console.log("✅ Connected to MongoDB Atlas (ES Modules)");
+        db = client.db(dbName);
+        return db;
+    } catch (err) {
+        console.error("❌ Failed to connect to MongoDB:", err);
+        throw err; 
+    }
+}
